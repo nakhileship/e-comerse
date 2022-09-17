39e12129f0371ef98441a8633cec9a7708992da6
nakhileship <www.nakhileshnakhu@gmail.com>
2022-09-17 22:04:46 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const vara = document.getElementById('bar');
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
 const nav = document.getElementById('navbar')
 
-if (vara) {
-    vara.addEventListener('click', () => {
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
         nav.classList.add('active');
     })
 }
